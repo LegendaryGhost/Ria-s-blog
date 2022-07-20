be73f36531b2cbaf3db41ceacf68bfb397beaba2
plinsy <plinsy2@gmail.com>
2022-07-20 17:07:21 +0300

    I prefere ria.blog

--- public/index.php
+++ public/index.php
@@ -1,29 +1,27 @@
 <?php
 
-    define('ROOT', dirname(__DIR__));
-    require ROOT . '/app/App.php';
-    App::load();
+define('ROOT', dirname(__DIR__));
+require ROOT . '/app/App.php';
+App::load();
 
-    if(isset($_GET['p'])){
-        extract($_GET);
-    } else {
-        $page = 'home';
-    }
+if (isset($_GET['p'])) {
+    extract($_GET);
+} else {
+    $page = 'home';
+}
 
-    ob_start();
-    switch($p){
-        case 'home' :
-            require ROOT . '/pages/posts/home.php';
-            break;
-        case 'posts.show' :
-            require ROOT . '/pages/posts/show.php';
-            break;
-        case 'posts.category' :
-            require ROOT . '/pages/posts/category.php';
-            break;
-    }
-    $content = ob_get_clean();
+ob_start();
+switch ($page) {
+    case 'home':
+        require ROOT . '/pages/posts/home.php';
+        break;
+    case 'posts.show':
+        require ROOT . '/pages/posts/show.php';
+        break;
+    case 'posts.category':
+        require ROOT . '/pages/posts/category.php';
+        break;
+}
+$content = ob_get_clean();
 
-    require ROOT . '/pages/templates/default.php';
-
-?>
+require ROOT . '/pages/templates/default.php';
